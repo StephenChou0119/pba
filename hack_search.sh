4ef5dee4e0fc949fdbeabe3fb998261a05de3d5e
sc <shortly_after@163.com>
2019-07-16 10:05:34 +0800

    add a hack_search.sh

---
+++ hack_search.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export PYTHONPATH="$(pwd)"
+
+python pba/search.py \
+    --local_dir "$PWD/results/" \
+    --model_name wrn_40_2 \
+    --num_samples 8 --perturbation_interval 3 --epochs 10 \
+    --name hack_first\
+    --lr 0.1 --wd 0.0005 --bs 512 --test_bs 512
+
