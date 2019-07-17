d29b641ce44038f41d8e757b18631304dcb11471
sc <shortly_after@163.com>
2019-07-17 14:44:04 +0800

    reduce search space

--- hack_search_resnet18.sh
+++ hack_search_resnet18.sh
@@ -4,9 +4,9 @@ export PYTHONPATH="$(pwd)"
 python pba/search.py \
     --local_dir "$PWD/results/" \
     --model_name resnet \
-    --num_samples 8 --perturbation_interval 3 --epochs 100 \
+    --num_samples 16 --perturbation_interval 3 --epochs 100 \
     --name resnet_100\
-    --lr 0.1 --wd 0.0005 --bs 32 --test_bs 32\
+    --lr 0.1 --wd 0.0005 --bs 48 --test_bs 48\
     --cpu 5\
     --gpu 1\
     --resnet_size 20\
