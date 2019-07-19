30dd8250fb1d380b5fd3c435524daaefe5fc39ce
sc <shortly_after@163.com>
2019-07-19 12:40:57 +0800

    update requirements

--- hack_search_efficientnetb0.sh
+++ hack_search_efficientnetb0.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH="$(pwd)"
 nohup \
 python pba/search.py \
-    --local_dir "$PWD/results/" \
+    --local_dir "/data/search_results/" \
     --model_name efficientnet-b0 \
     --num_samples 16 --perturbation_interval 3 --epochs 100 \
     --name efficientnetb0_search\
